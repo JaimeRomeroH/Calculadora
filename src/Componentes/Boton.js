96a02366ab64c2f8eeb8d4f2a4c8546fdad06f28
JaimeRomero <jaime_im@outlook.com>
2022-06-29 21:18:06 -0500

    Se modifico el archivo Boton.js

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "../Estilos/Boton.css"
 function Boton(props){
   const esOperador = valor =>{
     return isNaN(valor) && (valor != '.') && (valor != '=');
   };
    return(
      <div
-     className={`boton-contenedor ${esOperador(props.children) ? "operador" : null}`}>
+     className={`boton-contenedor ${esOperador(props.children) ? "operador" : ""}`.trimEnd()}>
        {props.children}
      </div>
    );
